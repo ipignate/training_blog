473cf3e69747a2e23f56bc39807f096901ac42a3
Rails Learning <ines.pignatelli@indiecampers.com>
2019-11-27 11:55:42 +0000

    Added show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,32 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(@article)
+            flash[:notice] = "Article was succesfully updated"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@articles)
+
+        if @article.save
+            flash[:notice] = "Article successfully created"
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
     end
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+    def show
+        @article = Article.find(params[:id])
     end
 end
