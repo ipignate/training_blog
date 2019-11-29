f6a6cd92b3cadab955194ae4d2da5423326d8fb3
Rails Learning <ines.pignatelli@indiecampers.com>
2019-11-29 14:09:29 +0000

    created user table and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    belongs_to :User, class_name: "User", foreign_key: "User_id"
 
     def index
         @articles = Article.all
